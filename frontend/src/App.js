f6c30cdeec5fe2e7e1f73e6de256b74076f8393b
Arul <143249822+Arul-1911@users.noreply.github.com>
2023-12-27 18:45:51 +0530

    intial client commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { Route,Routes, Navigate } from "react-router-dom";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Main from "./components/main";
+import ForgotPassword from "./components/forgotPassword";
+import PasswordReset from "./components/passwordResetPage";
+
+
+function App() {
+
+  const user  = localStorage.getItem("token")
+  return (
+    <>
+    <Routes>
+     {user && <Route path="/" element={<Main/>}/> }
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/forgot-password" element={<ForgotPassword/>}/>
+      <Route path="/api/password-reset/:id/:token" element={<PasswordReset/>}/>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/url/global.js
@@ -0,0 +1,3 @@
+const local_URL = "https://password-reset-i9q1.onrender.com/api";
+
+export default local_URL;
